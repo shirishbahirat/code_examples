6db38b1c558d4cb290efff4f9cf429cb2e266476
shirishbahirat <shirishbahirat@comcast.net>
2018-02-05 17:29:00 -0700

    test file for xor swap

---
+++ xorswap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+   int x = 10;
+   int y = 20;
+   
+   int addrN = 0000;
+   int addrA = 1111;
+   int addrB = 2222;
+   int addrC = 3333;
+   
+   std::cout << x << " " << y << std::endl;
+   
+   x^=y^=x^=y;
+   
+   std::cout << x << " " << y << std::endl;
+
+   int addrNNext = addrN ^ addrA;
+   int addrANext = addrA ^ addrB;
+
+   std::cout << (addrNNext ^ 0000) << " " << addrA << std::endl;
+
+   std::cout << (addrANext ^ (addrNNext ^ 0000)) << " " << addrB << std::endl;
+
+}
+
+
