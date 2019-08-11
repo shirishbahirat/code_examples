bf939c1f18b97fd3605a04cdc99b029f83b845ca
shirishbahirat <shirishbahirat@comcast.net>
2019-08-11 06:08:54 -0600

    Add files via upload

--- heap-sort.cpp
+++ heap-sort.cpp
@@ -1,77 +1,58 @@
 #include <iostream>
+
 using namespace std;
 
-void print_array(uint32_t arr[], uint32_t length)
-{
-
-	for (uint32_t i = 0; i < length; ++i)
-	{
-		cout << arr[i] << " " << flush;
-	}
-	cout << endl;
+void swap(int arr[], int a, int b) {
+  if (arr[a] != arr[b]) {
+    arr[a] ^= arr[b] ^= arr[a] ^= arr[b];
+  }
 }
 
-void swap(uint32_t arr[], uint32_t l, uint32_t h)
-{
-	if (l != h)
-	{
-		arr[l] ^= arr[h] ^= arr[l] ^= arr[h];
-	}
+void print_arr(int arr[], int length) {
+
+  for (int i = 0; i < length; ++i) {
+    cout << arr[i] << " ";
+  }
+
+  cout << endl;
 }
 
-void max_heap(uint32_t arr[], uint32_t length, uint32_t index)
-{
-	uint32_t parent = index;
-	uint32_t left = 2*index + 1;
-	uint32_t right = 2*index + 2;
+void heapify(int arr[], int index, int length) {
+  int largest = index;
+  int left = 2 * index + 1;
+  int right = 2 * index + 2;
 
-	if (left < length && arr[left] > arr[parent]) parent = left;
-	if (right < length && arr[right] > arr[parent]) parent = right;
+  if ((left < length) && (arr[left] > arr[largest]))
+    largest = left;
 
-	if(parent != index)
-	{
-		swap(arr, parent, index);
-		cout << "swap " << "index " << index << " " << flush;
-		print_array(arr, length);		
-		max_heap(arr, length, parent);
-	}
+  if ((right < length) && (arr[right] > arr[largest]))
+    largest = right;
 
+  if (largest != index) {
+    swap(arr, largest, index);
+    heapify(arr, largest, length);
+  }
 }
 
+void heap_sort(int arr[], int length) {
 
-void heap_sort(uint32_t arr[], uint32_t length)
-{
+  int parent = (length - 1) / 2;
 
-	for (int i = length/2-1; i >= 0; --i)
-	{
-		max_heap(arr, length, (uint32_t)i);
-		cout << "heap " << flush;
-		print_array(arr, length);
-	}
+  for (int index = parent; index >= 0; --index) {
+    heapify(arr, index, length);
+  }
 
-	for (int i = length - 1; i >= 0; --i)
-	{
-		swap(arr, 0, (uint32_t)i);
-		max_heap(arr, (uint32_t)i, 0);
-		cout << "sort " << flush;
-		print_array(arr, length);		
-	}
-
+  for (int idx = length - 1; idx >= 0; --idx) {
+    swap(arr, 0, idx);
+    heapify(arr, 0, idx);
+  }
 }
 
+int main(int argc, char const *argv[]) {
+  int arr[] = {10, 3, 4, 5, 6, 11, 8, 12};
 
+  heap_sort(arr, sizeof(arr) / sizeof(arr[0]));
 
-
-int main(int argc, const char* argv[])
-{
-	uint32_t arr[] = {10,2,4,12,25,41};
-	uint32_t length = sizeof(arr)/sizeof(uint32_t);
-
-	print_array(arr, length);
-	heap_sort(arr, length);
-
-    print_array(arr, length);
-
-
-	return 0;
+  print_arr(arr, sizeof(arr) / sizeof(arr[0]));
+  return 0;
 }
