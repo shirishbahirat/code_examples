f88f03090de0362ac5bc25e2b29ae273868a2797
shirishbahirat <shirishbahirat@comcast.net>
2019-08-11 20:35:55 -0600

    Add files via upload

--- quick-sort.cpp
+++ quick-sort.cpp
@@ -2,69 +2,54 @@
 
 using namespace std;
 
-
-void swap(uint32_t* idx1, uint32_t* idx2) 
-{ 
-
-    uint32_t temp = *idx1; 
-    *idx1 = *idx2; 
-    *idx2 = temp;
+void swap(uint32_t arr[], uint32_t a, uint32_t b) {
+  if (arr[a] != arr[b]) {
+    arr[a] ^= arr[b] ^= arr[a] ^= arr[b];
+  }
 }
 
+void print_array(uint32_t arr[], uint32_t length) {
 
-void print_array(uint32_t *arr, uint32_t len)
-{
-	for (int i = 0; i < len; ++i)
-	{
-		cout << *arr << " " << flush;
-		arr++;
-	}
-	cout << endl;
+  for (uint32_t i = 0; i < length; ++i) {
+    cout << arr[i] << " ";
+  }
+
+  cout << endl;
 }
 
-uint32_t quick_sort(uint32_t arr[], uint32_t low, uint32_t high)
-{
+uint32_t partation(uint32_t arr[], uint32_t low, uint32_t high) {
 
-	uint32_t pivot_current = arr[high];
-	int i = low;
+  uint32_t pivot = arr[high];
+  uint32_t index = low;
 
-	if (low < high)
-	{
-		for (int k = low; k < high; ++k)
-		{
-			if (arr[k] < pivot_current)
-			{
-				cout << k << ": " << flush;
-				swap(&arr[i], &arr[k]);
-			    ++i;
-				print_array(arr, 10);
-			}
+  for (uint32_t j = low; j < high; ++j) {
+    if (arr[j] < pivot) {
+      swap(arr, index, j);
+      ++index;
+    }
+  }
 
-		}
-		swap(&arr[i], &arr[high]);
+  swap(arr, index, high);
 
-	}
+  cout << pivot << " - ";
+  print_array(arr, 10);
 
-	return i;
-
+  return index;
 }
 
-int main(int argc, const char* argv[])
-{
-	uint32_t arr[] = {60, 20, 90, 70, 50, 100, 10, 80, 30, 40};
-	uint32_t len = sizeof(arr)/sizeof(uint32_t);
-	uint32_t next_pivot = len;
+void quick_sort(uint32_t arr[], uint32_t low, uint32_t high) {
+  if (low < high) {
+    uint32_t pivot = partation(arr, low, high);
+    if (pivot > 0)
+      quick_sort(arr, low, pivot - 1);
+    if (pivot < high)
+      quick_sort(arr, pivot + 1, high);
+  }
+}
 
-	print_array(arr, len);
-
-	while(next_pivot)
-	{
-		next_pivot = quick_sort(arr, 0, next_pivot-1);
-		quick_sort(arr, next_pivot,len-1);
-	}
-
-	print_array(arr, len);
-
-	return 0;
-
+int main(int argc, const char *argv[]) {
+  uint32_t arr[] = {10, 3, 4, 5, 6, 11, 8, 12, 1, 7};
+  quick_sort(arr, 0, (sizeof(arr) / sizeof(uint32_t)) - 1);
+  print_array(arr, (sizeof(arr) / sizeof(uint32_t)));
+  return 0;
 }
