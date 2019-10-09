4e44868f8bbc04b1fb6af5b3ea5b31c1ce790e59
shirishbahirat <shirishbahirat@comcast.net>
2019-10-08 20:52:09 -0600

    Add files via upload

--- llremove.cpp
+++ llremove.cpp
@@ -42,7 +42,38 @@ void print_node(ListNode *head) {
 
 class Solution {
 public:
-  ListNode *removeElements(ListNode *head, int val) { return head; }
+  ListNode *removeElements(ListNode *head, int val) {
+
+    ListNode *new_head = nullptr;
+
+    if (head == nullptr)
+      return head;
+
+    if (head->val == val) {
+      if (head->next) {
+        head = head->next;
+      } else {
+        return nullptr;
+      }
+      new_head = head->next;
+    } else {
+      new_head = head;
+    }
+
+    while (head->next) {
+      if (head->next->val == val) {
+        if (head->next) {
+          head->next = head->next->next;
+        } else {
+          head->next = nullptr;
+          break;
+        }
+      }
+      head = head->next;
+    }
+
+    return new_head;
+  }
 };
 
 int main(int argc, char const *argv[]) {
@@ -53,7 +84,7 @@ int main(int argc, char const *argv[]) {
 
   append(&head, 1);
   append(&head, 2);
-  append(&head, 3);
+  append(&head, 2);
   append(&head, 4);
   append(&head, 5);
   append(&head, 6);
