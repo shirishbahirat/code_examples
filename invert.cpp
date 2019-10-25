587d2abac564677632a0e23a7b9c3e1bba9e098f
shirishbahirat <shirishbahirat@comcast.net>
2019-10-24 20:18:08 -0600

    Add files via upload

--- invert.cpp
+++ invert.cpp
@@ -6,14 +6,31 @@ using namespace std;
 
 class Solution {
 public:
-  vector<vector<int>> flipAndInvertImage(vector<vector<int>> &A) { return A; }
-};
+  vector<vector<int>> flipAndInvertImage(vector<vector<int>> &A) {
 
+    for (int i = 0; i < A.size(); ++i) {
+      for (int j = 0, k = A[i].size() - 1; j < k; ++j, --k) {
+
+        int temp = A[i][j];
+        A[i][j] = A[i][k];
+        A[i][k] = temp;
+      }
+
+      for (int j = 0; j < A[i].size(); ++j) {
+
+        A[i][j] ^= 1;
+      }
+    }
+
+    return A;
+  }
+};
 int main(int argc, char const *argv[]) {
 
   Solution *sln = new Solution();
 
-  vector<vector<int>> items{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+  vector<vector<int>> items{
+      {1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}};
 
   sln->flipAndInvertImage(items);
 
