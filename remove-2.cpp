3b07400fc6f8e0ab54aae8bd6ebec1294959b5a2
shirishbahirat <shirishbahirat@comcast.net>
2019-12-08 13:34:15 -0700

    Add files via upload

--- remove-2.cpp
+++ remove-2.cpp
@@ -14,10 +14,17 @@ int main(int argc, char const *argv[]) {
 
   Solution *sln = new Solution();
 
-  vector<int> nums{0, 1, 2, 2, 3, 0, 4, 2};
-  // vector<int> nums{3};
+  // vector<int> nums{0, 1, 2, 2, 3, 0, 4, 2};
+  vector<int> nums{3};
 
-  sln->removeElement(nums, 2);
+  int val = sln->removeElement(nums, 3);
+
+  cout << val << endl;
+
+  for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
 
   return 0;
 }
