92f3ad9ca02fa114f413f3dd49be9d0b5fa69b7e
shirishbahirat <shirishbahirat@comcast.net>
2020-02-08 07:53:28 -0700

    Add files via upload

--- m-sort.cpp
+++ m-sort.cpp
@@ -2,30 +2,75 @@
 
 using namespace std;
 
-void print(int arr[], int len) {
-  for (int i = 0; i < len; ++len) {
-    cout << arr[i] << " ";
+void merge(int data[], int low, int mid, int high) {
+
+  int i = low;
+  int j = mid + 1;
+  int k = 0;
+  int as = mid - low + 1;
+  int bs = high - mid;
+  int len = as + bs;
+
+  cout << "merge " << len << endl;
+
+  int temp[len];
+
+  while ((i <= mid) && (j <= high)) {
+    if (data[i] < data[j]) {
+      temp[k++] = data[i++];
+    } else {
+      temp[k++] = data[j++];
+    }
+  }
+
+  cout << k << " " << j << " " << low << " " << high << endl;
+
+  for (; i <= mid;) {
+    temp[k++] = data[i++];
+  }
+
+  for (; j <= high;) {
+    temp[k++] = data[j++];
+  }
+
+  cout << "mdone ";
+  for (int x = low, y = 0; y < len; ++x, ++y) {
+    data[x] = temp[y];
+    cout << data[x] << " ";
   }
   cout << endl;
 }
 
-int merge_sort(int arr[], int low, int high) {
-  if (low < high) {
-    int mid = low + ((high - low) / 2);
-    merge_sort(arr, low, mid);
-    merge_sort(arr, mid + 1, high);
+void merge_sort(int data[], int low, int high) {
+
+  int mid = (low + high) / 2;
+
+  if (mid >= high)
+    return;
+
+  cout << "call " << low << " " << mid << " " << high << endl;
+
+  for (int i = low; i <= high; ++i) {
+    cout << data[i] << ' ';
   }
+  cout << endl;
+
+  merge_sort(data, low, mid);
+  merge_sort(data, mid + 1, high);
+  merge(data, low, mid, high);
 }
 
 int main(int argc, char const *argv[]) {
 
-  int arr[] = {9, 6, 3, 7, 4, 2, 8, 1, 5};
+  int data[] = {3, 4, 5, 11, 12, 8, 7, 6, 9, 2};
+  int len = (sizeof(data) / sizeof(int)) - 1;
 
-  int len = sizeof(arr) / sizeof(arr[0]);
+  merge_sort(data, 0, len);
 
-  merge_sort(arr, 0, (len - 1));
-
-  print(arr, len);
+  for (int i = 0; i <= len; ++i) {
+    cout << data[i] << ' ';
+  }
+  cout << endl;
 
   return 0;
 }
---
+++ merge.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  uint32_t a[] = {2, 5, 8, 9, 11};
+  uint32_t b[] = {1, 3, 4, 7};
+  uint32_t as = sizeof(a) / sizeof(a[0]);
+  uint32_t bs = sizeof(b) / sizeof(b[0]);
+  uint32_t cs = as + bs;
+  uint32_t c[cs];
+
+  uint32_t i = 0, j = 0, k = 0;
+
+  while ((i < as) && (j < bs)) {
+    if (a[i] < b[j]) {
+      c[k++] = a[i++];
+    } else {
+      c[k++] = b[j++];
+    }
+  }
+
+  for (; j < bs;) {
+    c[k++] = b[j++];
+  }
+
+  for (; i < as;) {
+    c[k++] = a[i++];
+  }
+
+  for (uint32_t m = 0; m < cs; ++m) {
+    cout << c[m] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
