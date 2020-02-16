e777cbe3f482964beac8d3603635d21d7bff0fbb
shirishbahirat <shirishbahirat@comcast.net>
2020-02-16 08:58:09 -0700

    Add files via upload

---
+++ ss.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int data[], int x, int y) {
+  int temp = data[x];
+  data[x] = data[y];
+  data[y] = temp;
+}
+
+int main(int argc, char const *argv[]) {
+
+  int data[] = {0, 3, 2, 2, 14, 8, 9, 12, 1, 5, 0, 13, 6, 10, 11, 25};
+  int len = (sizeof(data) / sizeof(data[0]));
+
+  for (int index = 0; index < len; ++index) {
+    int smallest = index;
+    cout << data[smallest] << " : ";
+    for (int idx = index; idx < len; ++idx) {
+      if (data[smallest] > data[idx]) {
+        smallest = idx;
+      }
+      cout << data[idx] << " ";
+    }
+    swap(data, index, smallest);
+    cout << ":" << data[smallest] << endl;
+  }
+
+  for (int i = 0; i < len; ++i)
+    cout << data[i] << " ";
+  cout << endl;
+
+  return 0;
+}
