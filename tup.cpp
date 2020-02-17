51bb7bbb3d2d53b2d858df1fb8aa55576ebf0d06
shirishbahirat <shirishbahirat@comcast.net>
2020-02-16 19:22:32 -0700

    Add files via upload

--- tup.cpp
+++ tup.cpp
@@ -2,10 +2,48 @@
 
 using namespace std;
 
+void swap(int data[], int x, int y)
+{
+  int temp = data[x];
+  data[x] = data[y];
+  data[y] = temp;
+}
+
+void trickle_up(int data[], int index)
+{
+
+  while (index)
+  {
+
+    int parent = (index - 1) / 2;
+
+    if (data[index] > data[parent])
+    {
+      swap(data, index, parent);
+    }
+
+    index = parent;
+  }
+}
+
+void heapify(int data[], int len)
+{
+  for (int i = 0; i <= len; ++i)
+    trickle_up(data, i);
+}
+
 int main(int argc, char const *argv[])
 {
-  int data[] = {10, 8, 6, 5, 3, 4, 2, 9};
-  int len = sizeof(data) / sizeof(data[0]);
+  int data[] = {6, 4, 8, 9, 3, 2, 10, 5};
+  int len = sizeof(data) / sizeof(data[0]) - 1;
+
+  heapify(data, len);
+
+  for (int i = 0; i <= len; ++i)
+  {
+    cout << data[i] << ' ';
+  }
+  cout << endl;
 
   return 0;
 }
